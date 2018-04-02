f19cfb38419662e27078ed539b0b994a0f2b3f3f
Raman Choudhary <raman.choudhary65@gmail.com>
2018-04-02 23:45:20 +0530

    added logging

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const shelljs = require('shelljs')
 const app = express();
 
 app.get("/", (req, res) => {
+  shelljs.echo("shelljs -->> ")
+  const ts = new Date().getTime()
+  shelljs.exec(`echo "${ts} ms" >> commit-log`)
+  
   res.send("Hello App! Works")
 })
 
