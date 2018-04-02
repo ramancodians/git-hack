57f383804e9c657f93d5ea9c85748ae8dfb261b7
Raman Choudhary <raman.choudhary65@gmail.com>
2018-04-03 00:01:54 +0530

    added logs

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const shelljs = require('shelljs')
 const app = express();
+let TASK = null;
 
 app.get("/", (req, res) => {
   shelljs.echo("shelljs -->> ")
   const ts = new Date().getTime()
   shelljs.exec(`echo "${ts} ms" >> commit-log`)
-  
   res.send("Hello App! Works")
+  console.log(process.env.ENV);
 })
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Listening at 5000");
 })
+
+function start(){
+  TASK = setInterval(() => {
+
+  }, 1000)
+}
