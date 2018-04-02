f920b26811f05e45408cbfa52854ace5a941b4fd
Raman Choudhary <raman.choudhary65@gmail.com>
2018-04-02 21:09:16 +0530

    added express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello JS");
+const express = require('express')
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello App! Works")
+})
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Listening at 5000");
+})
